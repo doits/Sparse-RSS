4be81ff2e7dc9686b57c31228aa223f5727ea4a8
Stefan Handschuh <handschuh.stefan@googlemail.com>
2010-12-13 22:45:12 +0000

    Removes unneeded permission. Version bump.
    git-svn-id: https://sparserss.googlecode.com/svn/trunk@72 63f75df1-c233-a9e8-a111-1e0e1c3d5386

--- src/de/shandschuh/sparserss/SDMountBroadcastReceiver.java
+++ src/de/shandschuh/sparserss/SDMountBroadcastReceiver.java
@@ -25,19 +25,27 @@
 
 package de.shandschuh.sparserss;
 
-import android.app.ActivityManager;
+import de.shandschuh.sparserss.service.RefreshService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.preference.PreferenceManager;
 
 public class SDMountBroadcastReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (!intent.getBooleanExtra("read-only", false)) {
-			ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+			android.os.Process.killProcess(android.os.Process.myPid());
 			
-			manager.restartPackage(Strings.PACKAGE);
+			try {
+				if (PreferenceManager.getDefaultSharedPreferences(context.createPackageContext(Strings.PACKAGE, 0)).getBoolean(Strings.SETTINGS_REFRESHENABLED, false)) {
+					context.startService(new Intent(context, RefreshService.class));
+				}
+			} catch (NameNotFoundException e) {
+				
+			}
 		}
 	}
 
