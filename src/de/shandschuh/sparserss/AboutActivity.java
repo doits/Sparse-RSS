40da9ce5b27d7c42b1a3376ea0f38ff010b53320
Stefan Handschuh <handschuh.stefan@googlemail.com>
2011-05-23 22:18:06 +0000

    Adds version number to title in about activity.
    git-svn-id: https://sparserss.googlecode.com/svn/trunk@125 63f75df1-c233-a9e8-a111-1e0e1c3d5386

--- src/de/shandschuh/sparserss/AboutActivity.java
+++ src/de/shandschuh/sparserss/AboutActivity.java
@@ -26,6 +26,7 @@
 package de.shandschuh.sparserss;
 
 import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 
 public class AboutActivity extends Activity {
@@ -33,6 +34,11 @@ public class AboutActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		try {
+			setTitle(new StringBuilder(getString(R.string.app_name)).append(' ').append(getPackageManager().getPackageInfo(getPackageName(), 0).versionName));
+		} catch (NameNotFoundException e) {
+
+		}
 		setContentView(R.layout.about);
 	}
 
