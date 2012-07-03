doits/Sparse-RSS

commit 015b0ff33a0fad208e3fccc07dff5e9b2957b896

Handle onPause and onResume, and also onSaveInstanceState and onRestoreInstanceState so that when we leave our activity (say, by clicking on a link) we'll have our web view's state information stored. Good for stuff like scrolling position and zoom factor.
Signed-off-by: Joel Low <joel@joelsplace.sg>

src/de/shandschuh/sparserss/EntryActivity.java
@@ -356,6 +356,13 @@ public class EntryActivity extends Activity {
 		
 		handler = new Handler();
 	}
+	
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState)
+	{
+		super.onRestoreInstanceState(savedInstanceState);
+		webView.restoreState(savedInstanceState);
+	}
 
 	@Override
 	protected void onResume() {
@@ -365,6 +372,7 @@ public class EntryActivity extends Activity {
 		}
 		uri = getIntent().getData();
 		parentUri = FeedData.EntryColumns.PARENT_URI(uri.getPath());
+		webView.onResume();
 		reload();
 	}
 	
@@ -690,8 +698,16 @@ public class EntryActivity extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
+		webView.onPause();
 		scrollX = webView.getScrollX();
 		scrollY = webView.getScrollY();
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		webView.saveState(outState);
+		super.onSaveInstanceState(outState);
 	}
 	
 	@Override

commit d94b6e3383d3a7f1cd1ec0edc6bbec65346c5a0f

Re-establishes pre-HC compatibility that has been broken with commit 015b0ff33a0fad208e3fccc07dff5e9b2957b896 .

src/de/shandschuh/sparserss/CompatibilityHelper.java
@@ -31,11 +31,16 @@ package de.shandschuh.sparserss;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.webkit.WebView;
 
 public class CompatibilityHelper {
 	private static final String METHOD_GETACTIONBAR = "getActionBar";
 	
 	private static final String METHOD_SETICON = "setIcon";
+	
+	private static final String METHOD_ONRESUME = "onResume";
+	
+	private static final String METHOD_ONPAUSE = "onPause";
 	
 	public static void setActionBarDrawable(Activity activity, Drawable drawable) {
 		try {
@@ -46,5 +51,21 @@ public class CompatibilityHelper {
 			
 		}
 
+	}
+	
+	public static void onResume(WebView webView) {
+		try {
+			WebView.class.getMethod(METHOD_ONRESUME).invoke(webView);
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	public static void onPause(WebView webView) {
+		try {
+			WebView.class.getMethod(METHOD_ONPAUSE).invoke(webView);
+		} catch (Exception e) {
+			
+		}
 	}
 }

src/de/shandschuh/sparserss/EntryActivity.java
@@ -372,7 +372,9 @@ public class EntryActivity extends Activity {
 		}
 		uri = getIntent().getData();
 		parentUri = FeedData.EntryColumns.PARENT_URI(uri.getPath());
-		webView.onResume();
+		if (MainTabActivity.POSTGINGERBREAD) {
+			CompatibilityHelper.onResume(webView);
+		}
 		reload();
 	}
 	
@@ -698,7 +700,9 @@ public class EntryActivity extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
-		webView.onPause();
+		if (MainTabActivity.POSTGINGERBREAD) {
+			CompatibilityHelper.onPause(webView);
+		}
 		scrollX = webView.getScrollX();
 		scrollY = webView.getScrollY();
 	}